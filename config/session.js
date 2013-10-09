603ed4e3db77b45c72cc6115a4f218d0d8f7f8e1
Kern0 <localhost@kern0.ru>
2013-10-09 06:17:21 +0400

    session fix

--- config/session.js
+++ config/session.js
@@ -11,47 +11,16 @@
  */
 const local = require('./local.js');
 
-module.exports.session = {
-
-  // Session secret is automatically generated when your new app is created
-  // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
-  secret: local.secret
+module.exports.session = { 
+	secret: local.secret
 
 
-  // In production, uncomment the following lines to set up a shared redis session store
-  // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
-  //
-  // The following values are optional, if no options are set a redis instance running
-  // on localhost is expected.
-  // Read more about options at: https://github.com/visionmedia/connect-redis
-  //
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
-  // prefix: 'sess:'
-
-
-  // Uncomment the following lines to use your Mongo adapter as a session store
-  // adapter: 'mongo',
-  //
-  // host: 'localhost',
-  // port: 27017,
-  // db: 'sails',
-  // collection: 'sessions',
-  //
-  // Optional Values:
-  //
-  // # Note: url will override other connection settings
-  // url: 'mongodb://user:pass@host:port/database/collection',
-  //
-  // username: '',
-  // password: '',
-  // auto_reconnect: false,
-  // ssl: false,
-  // stringify: true
+	adapter: 'redis',
+	host: local.redis.host,
+	port: local.redis.port,
+	ttl: local.redis.ttl,
+	db: local.redis.db,
+	pass: local.redis.pass,
+	prefix: local.redis.prefix
 
 };
