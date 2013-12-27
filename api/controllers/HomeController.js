06be3d308d2a10f524211177cbbb67385b55ad62
Kern0 <localhost@kern0.ru>
2013-12-28 02:05:25 +0400

    Мелкие синтаксические фиксы, добавил примечание по поводу теста.

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -1,8 +1,8 @@
 /**
  * HomeController
  *
- * @module    :: Controller
- * @description  :: Contains logic for handling requests.
+ * @module		:: Controller
+ * @description	:: Contains logic for handling requests.
  */
 var rand = require('generate-key');
 var crypto = require('crypto');
@@ -10,171 +10,172 @@ var path = require('path');
 
 function getStats(cb) {
 	async.waterfall([
-	function getCount(callback) {
-	 link.query('select count(1) from link',
-	    function(err, result) {
-		    if (err) return callback(err);
+		function getCount(callback) {
+			link.query('select count(1) from link',
+				function (err, result) {
+					if (err) return callback(err);
 
-		    callback(null, result[0]['count(1)']);
-	    });
-	},
-	function getVisitors(count, callback) {
-	 link.query('SELECT SUM(visitors) FROM link',
-	    function(err, result) {
-	       if (err) return callback(err);
+					callback(null, result[0]['count(1)']);
+				});
+		},
+		function getVisitors(count, callback) {
+			link.query('SELECT SUM(visitors) FROM link',
+				function(err, result) {
+					if (err) return callback(err);
 
-	       callback(null, {links: count, visitors: result[0]['SUM(visitors)']});
-	    });
-	}],
+					callback(null, {links: count, visitors: result[0]['SUM(visitors)']});
+				});
+		}
+	],
 	function (err, result) {
-	    if (err) return cb(err);
+		if (err) return cb(err);
 
-	    if (result.visitors === null) result.visitors = 0;
+		if (result.visitors === null) result.visitors = 0;
 
-	    cb(null, result);
+		cb(null, result);
 	});
 }
 
 module.exports = {
 
-   index: function(req,res){
-      res.etagify();
-      getStats(function(err, result) {
+	 index: function(req,res){
+			res.etagify();
+			getStats(function(err, result) {
 	 if (err) throw err;
 
 	 res.view({stats: result});
-      });
-   },
+			});
+	 },
 
-   shorten: function shorten(req,res){
-      res.etagify();
+	 shorten: function shorten(req,res){
+			res.etagify();
 
-      if (!req.body.url) {
+			if (!req.body.url) {
 	 getStats(function(err, result) {
-	    if (err) throw err;
-	    return res.view('home/index',{message: 'Введите URL!', stats: result});
+			if (err) throw err;
+			return res.view('home/index',{message: 'Введите URL!', stats: result});
 	 });
-      }
-      shorten.url = req.body.url;//Original URL
+			}
+			shorten.url = req.body.url;//Original URL
 
-      if (!req.body.name) {// Short URL
+			if (!req.body.name) {// Short URL
 	 shorten.sUrl = rand.generateKey(4);
-      } else {
+			} else {
 	 shorten.sUrl = req.body.name.replace(/[^a-zA-Z0-9-_]/g, '');
-      }
+			}
 
-      async.waterfall([
+			async.waterfall([
 	 function isExists(callback) {
-	    link.findOne({
-	       shortURL: shorten.sUrl
-	    }).done(function(err, link) {
-	       if (err) callback(err);
+			link.findOne({
+				 shortURL: shorten.sUrl
+			}).done(function(err, link) {
+				 if (err) callback(err);
 
-	       if (link) {
-		  callback({msg: 'Увы, но этот короткий адрес уже занят', show: true});
-	       } else {
-		  callback(null);
-	       }
-	    });
+				 if (link) {
+			callback({msg: 'Увы, но этот короткий адрес уже занят', show: true});
+				 } else {
+			callback(null);
+				 }
+			});
 	 },
 	 function createLink(callback) {
-	    shorten.delink = crypto.createHash('sha1').update(shorten.sUrl + rand.generateKey(8)).digest('hex');
+			shorten.delink = crypto.createHash('sha1').update(shorten.sUrl + rand.generateKey(8)).digest('hex');
 
-	    link.create({
-	       originalURL: shorten.url,
-	       shortURL: shorten.sUrl,
-	       visitors: '0',
-	       delink: shorten.delink
-	    }).done(function(err, link) {
-	       if (err) callback(err);
+			link.create({
+				 originalURL: shorten.url,
+				 shortURL: shorten.sUrl,
+				 visitors: '0',
+				 delink: shorten.delink
+			}).done(function(err, link) {
+				 if (err) callback(err);
 
-	       callback(null, {originalURL: shorten.url, shortURL: shorten.sUrl, delink: shorten.delink});
-	    });
+				 callback(null, {originalURL: shorten.url, shortURL: shorten.sUrl, delink: shorten.delink});
+			});
 	 }],
 	 function(err, result) {
-	    if (err) {
-	       if (err.show) {
-		  return res.view('home/index', {message: err.msg});
-	       } else {
-		  throw err;
-		  return;
-	       }
-	    }
+			if (err) {
+				 if (err.show) {
+			return res.view('home/index', {message: err.msg});
+				 } else {
+			throw err;
+			return;
+				 }
+			}
 
-	    res.view('home/shorten',result);
+			res.view('home/shorten',result);
 	 }
-      );
-   },
+			);
+	 },
 
-   shorted: function(req,res) {
-    if (req.url == '/') return res.redirect('/');
-    res.etagify();
-    link.findOne({
-      shortURL: req.param('shortURL')
-    }).done(function(err, link) {
-      if (!_.isObject(link)) return res.redirect('/');
-      if (err) {
+	 shorted: function(req,res) {
+		if (req.url == '/') return res.redirect('/');
+		res.etagify();
+		link.findOne({
+			shortURL: req.param('shortURL')
+		}).done(function(err, link) {
+			if (!_.isObject(link)) return res.redirect('/');
+			if (err) {
 	return res.redirect('/');
-      } else {
+			} else {
 	link.visitors++;
 	link.save(function(err) {
-	  if (err)  console.error('[ERROR] Can\'t save visitor at', Date(),' with err:\n',err)
+		if (err)	console.error('[ERROR] Can\'t save visitor at', Date(),' with err:\n',err)
 	});
 	res.redirect(link.originalURL);
-      }
-    });
-   },
+			}
+		});
+	 },
 
-   info: function(req,res) {
-    if (req.url == '/') return res.redirect('/');
-    res.etagify();
-    link.findOne({
-      shortURL: req.param('shortURL')
-    }).done(function(err, link) {
-      if (!_.isObject(link)) return res.redirect('/');
-      if (err) {
+	 info: function(req,res) {
+		if (req.url == '/') return res.redirect('/');
+		res.etagify();
+		link.findOne({
+			shortURL: req.param('shortURL')
+		}).done(function(err, link) {
+			if (!_.isObject(link)) return res.redirect('/');
+			if (err) {
 	return res.redirect('/');
-      } else {
+			} else {
 	res.view({originalURL: link.originalURL, shortURL: link.shortURL, createdAt: link.createdAt, visitors: link.visitors});
-      }
-    });
-   },
+			}
+		});
+	 },
 
-   delinkTpl: function delinkTpl(req,res) {
-   res.etagify();
-   link.findOne({
-      delink: req.param('delink')
-   }).done(function(err, link) {
-      if (err) throw err;
-      if (!_.isObject(link)) return res.redirect('/');
+	 delinkTpl: function delinkTpl(req,res) {
+	 res.etagify();
+	 link.findOne({
+			delink: req.param('delink')
+	 }).done(function(err, link) {
+			if (err) throw err;
+			if (!_.isObject(link)) return res.redirect('/');
 
-      res.view('home/delink',{shortURL: link.shortURL, delink: link.delink});
-   });
-   },
+			res.view('home/delink',{shortURL: link.shortURL, delink: link.delink});
+	 });
+	 },
 
-   delink: function delink(req,res) {
-      if (req.param('confirm') === 'yeas') {
+	 delink: function delink(req,res) {
+			if (req.param('confirm') === 'yeas') {
 	 link.destroy(function(err) {
-	    if (err) {
-	       throw err;
-	    }
+			if (err) {
+				 throw err;
+			}
 	 });
 	 res.json({code: 204});
-      } else {
+			} else {
 	 res.redirect('/');
-      }
-   },
+			}
+	 },
 
-   favicon: function(req,res) {
-      res.sendfile('assets/favicon.ico');
-   },
+	 favicon: function(req,res) {
+			res.sendfile('assets/favicon.ico');
+	 },
 
-   robots: function(req,res) {
-      res.sendfile('assets/robots.txt');
-   },
+	 robots: function(req,res) {
+			res.sendfile('assets/robots.txt');
+	 },
 
-   about: function(req,res) {
-      res.etagify();
-      res.view('home/about');
-   }
+	 about: function(req,res) {
+			res.etagify();
+			res.view('home/about');
+	 }
 };
