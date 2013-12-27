624253427a2bf5474956f171eb637aca7e1d8361
Kern0 <localhost@kern0.ru>
2013-12-28 02:40:25 +0400

    Фикс вылета при отсутствии URL

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -39,104 +39,107 @@ function getStats(cb) {
 module.exports = {
 
 	 index: function(req,res){
-			res.etagify();
-			getStats(function(err, result) {
-	 if (err) throw err;
+		res.etagify();
+		getStats(function(err, result) {
+			if (err) throw err;
 
-	 res.view({stats: result});
-			});
+			res.view({stats: result});
+		});
 	 },
 
 	 shorten: function shorten(req,res){
-			res.etagify();
+		res.etagify();
 
-			if (!req.body.url) {
-	 getStats(function(err, result) {
-			if (err) throw err;
-			return res.view('home/index',{message: 'Введите URL!', stats: result});
-	 });
-			}
-			shorten.url = req.body.url;//Original URL
+		shorten.url = req.body.url;//Original URL
 
-			if (!req.body.name) {// Short URL
-	 shorten.sUrl = rand.generateKey(4);
-			} else {
-	 shorten.sUrl = req.body.name.replace(/[^a-zA-Z0-9-_]/g, '');
-			}
+		if (!req.body.name) {// Short URL
+			shorten.sUrl = rand.generateKey(4);
+		} else {
+			shorten.sUrl = req.body.name.replace(/[^a-zA-Z0-9-_]/g, '');
+		}
 
-			async.waterfall([
-	 function isExists(callback) {
-			link.findOne({
-				 shortURL: shorten.sUrl
-			}).done(function(err, link) {
-				 if (err) callback(err);
+		async.waterfall([
+			function isExists(callback) {
+				if (!shorten.url) {
+					return callback({msg: 'Введите URL!', show: true});
+				}
+				link.findOne({
+					shortURL: shorten.sUrl
+				}).done(function(err, link) {
+					if (err) return callback(err);
 
-				 if (link) {
-			callback({msg: 'Увы, но этот короткий адрес уже занят', show: true});
-				 } else {
-			callback(null);
-				 }
-			});
-	 },
-	 function createLink(callback) {
-			shorten.delink = crypto.createHash('sha1').update(shorten.sUrl + rand.generateKey(8)).digest('hex');
+					if (link) {
+						callback({msg: 'Увы, но этот короткий адрес уже занят', show: true});
+					} else {
+						callback(null);
+					}
+				});
+			 },
+			 function createLink(callback) {
+					shorten.delink = crypto.createHash('sha1').update(shorten.sUrl + rand.generateKey(8)).digest('hex');
 
-			link.create({
-				 originalURL: shorten.url,
-				 shortURL: shorten.sUrl,
-				 visitors: '0',
-				 delink: shorten.delink
-			}).done(function(err, link) {
-				 if (err) callback(err);
+					link.create({
+						originalURL: shorten.url,
+						shortURL: shorten.sUrl,
+						visitors: '0',
+						delink: shorten.delink
+					}).done(function(err, link) {
+						if (err) return callback(err);
 
-				 callback(null, {originalURL: shorten.url, shortURL: shorten.sUrl, delink: shorten.delink});
-			});
-	 }],
-	 function(err, result) {
-			if (err) {
-				 if (err.show) {
-			return res.view('home/index', {message: err.msg});
-				 } else {
-			throw err;
-			return;
-				 }
-			}
+						callback(null, {originalURL: shorten.url, shortURL: shorten.sUrl, delink: shorten.delink});
+					});
+			 }],
+			 function(err, result) {
+					if (err) {
+						 if (err.show) {
+							return res.view('home/index', {message: err.msg});
+						 } else {
+							throw err;
+							return;
+						 }
+					}
 
-			res.view('home/shorten',result);
-	 }
-			);
+					res.view('home/shorten',result);
+			 }
+		);
 	 },
 
 	 shorted: function(req,res) {
 		if (req.url == '/') return res.redirect('/');
+
 		res.etagify();
+
 		link.findOne({
 			shortURL: req.param('shortURL')
 		}).done(function(err, link) {
 			if (!_.isObject(link)) return res.redirect('/');
+
 			if (err) {
-	return res.redirect('/');
+				return res.redirect('/');
 			} else {
-	link.visitors++;
-	link.save(function(err) {
-		if (err)	console.error('[ERROR] Can\'t save visitor at', Date(),' with err:\n',err)
-	});
-	res.redirect(link.originalURL);
+				link.visitors++;
+				link.save(function(err) {
+					if (err) console.error('[ERROR] Can\'t save visitor at', Date(),' with err:\n',err)
+				});
+				res.redirect(link.originalURL);
 			}
 		});
 	 },
 
 	 info: function(req,res) {
 		if (req.url == '/') return res.redirect('/');
+
 		res.etagify();
+
 		link.findOne({
 			shortURL: req.param('shortURL')
 		}).done(function(err, link) {
 			if (!_.isObject(link)) return res.redirect('/');
+
 			if (err) {
-	return res.redirect('/');
+				return res.redirect('/');
 			} else {
-	res.view({originalURL: link.originalURL, shortURL: link.shortURL, createdAt: link.createdAt, visitors: link.visitors});
+				res.view({originalURL: link.originalURL, shortURL: link.shortURL, createdAt: link.createdAt, visitors: link.visitors});
 			}
 		});
 	 },
